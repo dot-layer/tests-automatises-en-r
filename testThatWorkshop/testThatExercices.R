2dfc0e89cfff2fe080721aca0513b774cb5c6808
davebulaval <daveid.beauchemin.5@ulaval.ca>
2019-03-15 21:24:20 -0400

    added exercice of testthat book

---
+++ testThatWorkshop/testThatExercices.R
@@ -0,0 +1,5 @@
+install.packages("assertive", "BiocCheck", "caret", "covr", "cyclocomp", "data.table", 
+                 "devtools", "digest", "dplyr", "ggplot2", "installr", "knitr", "magrittr", 
+                 "plyr", "Rcpp", "rebus.datetimes", "roxygen2", "RPostgreSQL", "runittotestthat",
+                 "sig", "testthat", "withr")
+
